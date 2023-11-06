586d5070a4c610880aa55545bd51297e39987084
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2023-11-06 11:19:14 +0100

    Completed points 1-4

---
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -0,0 +1,49 @@
+package it.unibo.inner.impl;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import it.unibo.inner.api.IterableWithPolicy;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
+
+    final private T elements[];
+
+    public IterableWithPolicyImpl(final T elements[]) {
+        this.elements = elements;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Inner();
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate filter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
+    }
+
+    private class Inner implements Iterator<T> {
+
+        private int current;
+
+        @Override
+        public boolean hasNext() {
+            return this.current < Objects.requireNonNull(elements).length ? true : false;
+        }
+
+        @Override
+        public T next() {
+            if (this.hasNext()) {
+                this.current++;
+                return elements[this.current];
+            }
+            return null;
+        }
+        
+    } 
+    
+}
